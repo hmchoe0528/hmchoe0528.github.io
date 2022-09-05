0d2bc49dff9e94629ca46d6e99ad144c8c51a547
Hyeongmin Choe <sixtail528@snu.ac.kr>
2022-09-05 16:29:25 +0200

    birth updated

--- js/javascript.js
+++ js/javascript.js
@@ -87,8 +87,8 @@ function loadFooter() {
 function updateAge(){
 	if( $('.update-birth').length > 0 ){
 		var myAge;
-		// var birth = new Date(Date.parse('1994 Sep 21 0:0:0 UTC')).getTime();
-		var birth = new Date(Date.parse('1994/9/21')).getTime();
+		// var birth = new Date(Date.parse('1994 May 28 0:0:0 UTC')).getTime();
+		var birth = new Date(Date.parse('1994/5/28')).getTime();
 		setInterval(function(){
 			myAge = new Number(new Date(new Date().getTime() - birth) / (1000 * 60 * 60 * 24 * 365.25)).toFixed(10);
 			$('.update-birth p').text(myAge);
